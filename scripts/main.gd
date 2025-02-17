cc3e1d5af490e54214b2faa56cb0e0ddc64c52de
Benicio Franqui <43625470+Benicio26@users.noreply.github.com>
2025-02-17 12:20:10 +0100

    Update main
    Update main + removed unused addon

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,12 @@
 extends Node3D
 @onready var camera_3d: Camera3D = $player/head/Camera3D
 @onready var object_detection_ray: RayCast3D = $player/head/Camera3D/object_detection_ray
-@onready var computer_shop = preload("res://scenes/computer_shop.tscn")
 # game states vars
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -20,5 +19,6 @@ func object_detection():
 		var encountered_object = object_detection_ray.get_collider()
 		# Check what object is being collided with
 		if encountered_object.is_in_group("OpensShop"):
+			var computer_shop = preload("res://scenes/computer_shop.tscn")
 			var computer_shop_node = computer_shop.instantiate()
 			add_child(computer_shop_node)
